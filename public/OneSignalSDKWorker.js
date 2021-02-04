5c9b09aff3e87059ebb6c166e47d8877beb2952f
elliotoconnor <5060405@rts-365.dk>
2021-02-04 13:23:48 +0100

    hej

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -1,2 +1,2 @@
-importScripts("htt")
+importScripts("https://bennys-bageri.netlify.app/");
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Router } from '@reach/router';
 import './App.css';
+import OneSignal from 'react-onesignal';
+
 import Products from './components/Products';
 import Notification from './components/Notification';
 //import { library } from "@fortawesome/fontawesome-svg-core";
@@ -8,14 +10,15 @@ import Notification from './components/Notification';
 //import { fa-shopping-basket, fa-bars } from "@fortawesome/free-solid-svg-icons";
 //import { fa-shopping-basket, fa-bars } from "@fortawesome/free-regular-svg-icons";
 
+OneSignal.initialize('2f4f9432-f62c-45f0-91a1-2d5ef683666f', {
+  safari_web_id:"web.onesignal.auto.694eada3-a476-4a33-8c61-99aa6c1d60b0",
+});
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Products path="/"/>
-      </Router>
-      <Router >
-        <Notification path="/admin" />
       </Router>
     </div>
   );
