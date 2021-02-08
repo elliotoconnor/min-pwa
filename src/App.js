279ec241c5632dcf5c7e71c8d7481fd37cde1f37
elliotoconnor <5060405@rts-365.dk>
2021-02-08 12:38:09 +0100

    offline added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Route, Redirect, Switch, } from "react-router";
+import { Router } from "@reach/router";
 import './App.css';
 import OneSignal from 'react-onesignal';
 import Products from './components/Products';
 import Notification from './components/Notification';
+import Offline from './components/Notfound';
 
 OneSignal.initialize("0b523ce6-7311-4040-9a01-3f4ed01c3429");
 
 function App() {
   return (
-    <Switch>
       <div className="App">
-        <Route exact path="/">
-          <Products />
-        </Route>
-        <Route path="admin">
-          <Notification />
-        </Route>
-        <Redirect from="/" to="offline.html" />
+        <Router>
+            <Products path="/" />
+            <Notification path="admin" />
+            <Offline default />
+        </Router>
+          
       </div>
-    </Switch>
+    
   );
 } 
 
---
+++ src/components/Notfound.js
@@ -0,0 +1,9 @@
+const Notfound = () => {
+    return (
+        <div>
+            <h1>This is offline site</h1>
+        </div>
+    )
+}
+
+export default Notfound
